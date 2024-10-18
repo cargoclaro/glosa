34b2fd135512e8ba31e008cbf9c0f2043f331d96
Macias Romero Jorge Humberto <macias_george@outlook.com>
2024-10-18 13:04:05 -0600

    [UPDATE] gloss analysis better structure

--- app/(dashboard)/gloss/[id]/analysis/components/SavedNFinish.tsx
+++ app/(dashboard)/gloss/[id]/analysis/components/SavedNFinish.tsx
@@ -1,10 +1,33 @@
 import { GenericCard } from "@/app/components";
+import formatCurrency from "@/app/utils/format-currency";
+import { CurrencyDollar, RightArrow } from "@/public/icons";
 
 const SavedNFinish = () => {
   return (
-    <GenericCard customClass="bg-green-300 w-full h-[300px]">
-      SavedAndFinish
-    </GenericCard>
+    <div className="flex flex-col gap-4">
+      <GenericCard>
+        <div className="flex justify-between gap-1">
+          <p
+            title={"+" + formatCurrency(5200, "MXN")}
+            className="text-2xl font-bold truncate text-green-500"
+          >
+            {"+" + formatCurrency(5200, "MXN")}
+          </p>
+          <div className="h-full p-3 rounded-full bg-green-500 text-white">
+            <CurrencyDollar />
+          </div>
+        </div>
+        <small>Ahorrado en multas</small>
+      </GenericCard>
+      <div className="mx-auto">
+        <button className="px-12 py-2 rounded-md border border-black text-sm relative group hover:bg-gray-100 transition-colors duration-200">
+          Terminar
+          <span className="absolute right-1 top-1.5 group-hover:animate-fade-right animate-infinite animate-duration-[2000ms] animate-alternate">
+            <RightArrow />
+          </span>
+        </button>
+      </div>
+    </div>
   );
 };
 
