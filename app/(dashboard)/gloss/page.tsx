ccc18f4a887509873068deaa3966e8a703db4c35
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2025-02-26 17:24:12 +0000

    Refactor gloss page to use direct Prisma query with user authentication

--- app/(dashboard)/gloss/page.tsx
+++ app/(dashboard)/gloss/page.tsx
@@ -1,13 +1,21 @@
+import { isAuthenticated } from "@/app/shared/services/auth";
 import { GlossDataTable, GlossDataTableColumns } from "./components";
-import { getMyAnalysis } from "@/app/shared/services/customGloss/controller";
 import type { Metadata } from "next";
+import prisma from "@/app/shared/services/prisma";
 
 export const metadata: Metadata = {
   title: "Operaciones",
 };
 
 const GlossPage = async () => {
-  const myGlosses = (await getMyAnalysis());
+  const session = await isAuthenticated();
+  const userId = session["userId"];
+  if (typeof userId !== "string") {
+    throw new Error("User ID is not a string");
+  }
+  const myGlosses = await prisma.customGloss.findMany({
+    where: { userId },
+  });
   if (!myGlosses) {
     return <div>No se encontraron operaciones</div>;
   }
