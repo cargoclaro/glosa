f11888274f78b30f2e4b813ef385c2d1d2c19d7e
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2025-03-10 18:32:38 +0000

    Update COVE Date Format Description in Pedimento Validation
    - Clarify date format description in `validateFechasYFolios`
    - Correct date format notation from YYYYMMDD to YYYY/MM/DD
    - Maintain consistent description of date formats for facturas, pedimentos, and COVE

--- app/shared/services/customGloss/glosa/pedimento/validation_steps_impo/6.datos-de-factura.ts
+++ app/shared/services/customGloss/glosa/pedimento/validation_steps_impo/6.datos-de-factura.ts
@@ -191,7 +191,7 @@ export async function validateFechasYFolios(pedimento: Pedimento, cove: Cove, in
   
   const validation = {
     name: "Validación de fechas de emisión y número de COVE",
-    description: "Verificar secuencias lógicas y coincidencias exactas:\n\nFechas:\n• Fecha emisión Factura debe ser menor o igual a la Fecha entrada Pedimento\n• Fecha COVE debe ser igual a la Fecha Factura\n\nNúmeros:\n• Número COVE en el Pedimento debe ser igual al Número COVE en el COVE\n• Las facturas normalmente vienen en formato americano, YYYYMMDD, el pedimento y COVE viene en formato mexicano, MMDDYYYY",
+    description: "Verificar secuencias lógicas y coincidencias exactas:\n\nFechas:\n• Fecha emisión Factura debe ser menor o igual a la Fecha entrada Pedimento\n• Fecha COVE debe ser igual a la Fecha Factura\n\nNúmeros:\n• Número COVE en el Pedimento debe ser igual al Número COVE en el COVE\n• Las facturas normalmente vienen en formato americano, YYYY/MM/DD, el pedimento y COVE viene en formato mexicano, DD/MM/YYYY",
     contexts: {
       [CustomGlossTabContextType.PROVIDED]: {
         "Pedimento": {
