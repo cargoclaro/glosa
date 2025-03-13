eb44408ba64ad068b22aa5fe31d4730de4ba8f8c
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2025-03-13 14:40:11 +0000

    Enhance XML to JSON conversion and update CFDI schema
    - Add a new script for converting XML data to JSON format, utilizing `fast-xml-parser` and saving the output to `output.json`.
    - Update the CFDI schema to include validation for `cfdi:Conceptos` and its array of `cfdi:Concepto` objects, ensuring proper structure for invoice data.
    - Refactor file handling in the XML parsing script for improved clarity and efficiency.

--- app/(dashboard)/home/components/Header/glosa/schemas/cfdi.ts
+++ app/(dashboard)/home/components/Header/glosa/schemas/cfdi.ts
@@ -3,6 +3,11 @@ import { z } from "zod"
 export const cfdiSchema = z.object({
   "?xml": z.object({ version: z.string(), encoding: z.string() }),
   "cfdi:Comprobante": z.object({
+    "cfdi:Conceptos": z.object({
+      "cfdi:Concepto": z.array(z.object({
+        Cantidad: z.string(),
+      })),
+    }),
     "cfdi:Complemento": z.object({
       "tfd:TimbreFiscalDigital": z.object({
         UUID: z.string(),
--- app/(dashboard)/home/components/Header/glosa/xml-to-json.ts
+++ app/(dashboard)/home/components/Header/glosa/xml-to-json.ts
@@ -1,4 +1,5 @@
 import { XMLParser } from "fast-xml-parser";
+import { writeFileSync } from "fs";
 
 const xmlUrl = "https://jsht6r4dkc.ufs.sh/f/sP56sMGH6Y15DShVFnWvXf1H4BGpPhqDTbi9LIkRoZlxA503"
 
@@ -11,8 +12,7 @@ async function parseXml() {
   const file = await fetch(xmlUrl);
   const xmlData = await file.text();
   const jsonData = parser.parse(xmlData, true);
-  const fs = require('fs');
-  fs.writeFileSync('output.json', JSON.stringify(jsonData, null, 2));
+  writeFileSync('output.json', JSON.stringify(jsonData, null, 2));
 }
 
 parseXml();
