faa47f290e894d68a25c4a80b3631aa272c2f45f
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2025-03-25 00:35:16 +0000

    Remove Pediment component and update exports for cleaner code structure
    - Deleted the Pediment component to streamline the codebase and eliminate unused functionality.
    - Updated exports in the index file to reflect the removal of the Pediment component, ensuring only relevant components are exported.

--- app/(dashboard)/gloss/[id]/analysis/components/index.ts
+++ app/(dashboard)/gloss/[id]/analysis/components/index.ts
@@ -1,7 +1,6 @@
 import Analysis from './Analysis';
 import Documents from './Documents';
-import Pediment from './Pediment';
 import PedimentAnalysisNFinish from './PedimentAnalysisNFinish';
 import SavedNFinish from './SavedNFinish';
 
-export { Pediment, Analysis, Documents, SavedNFinish, PedimentAnalysisNFinish };
+export { Analysis, Documents, SavedNFinish, PedimentAnalysisNFinish };
