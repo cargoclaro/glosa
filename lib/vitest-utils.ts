de9e343c24b832f04e622f3e9777121d9a0ab061
PedroAVJ <pedroantoniovillanuevajuarez@gmail.com>
2025-03-27 20:30:11 +0000

    Refactor utility functions by moving expectToBeDefined to vitest-utils
    - Removed the expectToBeDefined function from utils.ts and added it to a new vitest-utils.ts file for better organization.
    - This change enhances code clarity by separating utility functions from testing assertions.

---
+++ lib/vitest-utils.ts
@@ -0,0 +1,9 @@
+import { expect } from 'vitest';
+
+// https://github.com/vitest-dev/vitest/issues/2883#issuecomment-1897897016
+export function expectToBeDefined<T>(
+  value: T | undefined
+): asserts value is T {
+  // biome-ignore lint/suspicious/noMisplacedAssertion: This is a helper function to narrow the type
+  expect(value).toBeDefined();
+}
