9506389fbdf9d4b7d3a3727e89871779df4ecdf3
Macías Romero <macias_george@outlook.com>
2025-04-03 14:04:02 -0600

    [UPDATE] new url field to db schema in CustomGlossTabContext

--- db/schema.ts
+++ db/schema.ts
@@ -1,8 +1,6 @@
-import type {
-  Pedimento,
-} from '@/shared/services/customGloss/data-extraction/schemas';
-import type { Cove } from '@/shared/services/customGloss/extract-and-structure/schemas';
-import { relations } from 'drizzle-orm';
+import type { Pedimento } from "@/shared/services/customGloss/data-extraction/schemas";
+import type { Cove } from "@/shared/services/customGloss/extract-and-structure/schemas";
+import { relations } from "drizzle-orm";
 import {
   boolean,
   doublePrecision,
@@ -17,70 +15,70 @@ import {
   timestamp,
   uniqueIndex,
   uuid,
-} from 'drizzle-orm/pg-core';
+} from "drizzle-orm/pg-core";
 
-export const OperationStatus = pgEnum('OperationStatus', [
-  'IN_PROGRESS',
-  'DONE',
+export const OperationStatus = pgEnum("OperationStatus", [
+  "IN_PROGRESS",
+  "DONE",
 ]);
 
-export const CustomGlossType = pgEnum('CustomGlossType', [
-  'LOW',
-  'HIGH',
-  'MEDIUM',
+export const CustomGlossType = pgEnum("CustomGlossType", [
+  "LOW",
+  "HIGH",
+  "MEDIUM",
 ]);
 
 export const customGlossContextTypes = [
-  'PROVIDED',
-  'INFERRED',
-  'EXTERNAL',
+  "PROVIDED",
+  "INFERRED",
+  "EXTERNAL",
 ] as const;
 
 export type CustomGlossTabContextTypes =
   (typeof customGlossContextTypes)[number];
 
 export const CustomGlossTabContextType = pgEnum(
-  'CustomGlossTabContextType',
+  "CustomGlossTabContextType",
   customGlossContextTypes
 );
 
-export const CustomGloss = pgTable('CustomGloss', {
-  id: uuid('id').notNull().primaryKey().defaultRandom(),
-  summary: text('summary').notNull(),
-  timeSaved: doublePrecision('timeSaved').notNull(),
-  moneySaved: doublePrecision('moneySaved').notNull(),
-  importerName: text('importerName').notNull(),
-  operationStatus: OperationStatus('operationStatus')
+export const CustomGloss = pgTable("CustomGloss", {
+  id: uuid("id").notNull().primaryKey().defaultRandom(),
+  summary: text("summary").notNull(),
+  timeSaved: doublePrecision("timeSaved").notNull(),
+  moneySaved: doublePrecision("moneySaved").notNull(),
+  importerName: text("importerName").notNull(),
+  operationStatus: OperationStatus("operationStatus")
     .notNull()
-    .default('IN_PROGRESS'),
-  userId: text('userId').notNull(),
+    .default("IN_PROGRESS"),
+  userId: text("userId").notNull(),
   cove: json().$type<Cove>(),
   pedimento: json().$type<Pedimento>(),
-  createdAt: timestamp('createdAt', { precision: 3 }).notNull().defaultNow(),
+  createdAt: timestamp("createdAt", { precision: 3 }).notNull().defaultNow(),
 });
 
 export type CustomGlossTable = typeof CustomGloss.$inferSelect;
 
 export const CustomGlossAlert = pgTable(
-  'CustomGlossAlert',
+  "CustomGlossAlert",
   {
-    id: serial('id').notNull(),
-    type: CustomGlossType('type').notNull(),
-    description: text('description').notNull(),
-    customGlossId: uuid('customGlossId').notNull(),
-    createdAt: timestamp('createdAt', { precision: 3 }).notNull().defaultNow(),
-    updatedAt: timestamp('updatedAt', { precision: 3 }).notNull(),
+    id: serial("id").notNull(),
+    type: CustomGlossType("type").notNull(),
+    description: text("description").notNull(),
+    customGlossId: uuid("customGlossId").notNull(),
+    createdAt: timestamp("createdAt", { precision: 3 }).notNull().defaultNow(),
+    updatedAt: timestamp("updatedAt", { precision: 3 }).notNull(),
   },
   (CustomGlossAlert) => ({
     CustomGlossAlert_customGloss_fkey: foreignKey({
-      name: 'CustomGlossAlert_customGloss_fkey',
+      name: "CustomGlossAlert_customGloss_fkey",
       columns: [CustomGlossAlert.customGlossId],
       foreignColumns: [CustomGloss.id],
     })
-      .onDelete('cascade')
-      .onUpdate('cascade'),
+      .onDelete("cascade")
+      .onUpdate("cascade"),
     CustomGlossAlert_cpk: primaryKey({
-      name: 'CustomGlossAlert_cpk',
+      name: "CustomGlossAlert_cpk",
       columns: [CustomGlossAlert.id, CustomGlossAlert.customGlossId],
     }),
   })
@@ -89,25 +87,25 @@ export const CustomGlossAlert = pgTable(
 export type CustomGlossAlertTable = typeof CustomGlossAlert.$inferSelect;
 
 export const CustomGlossFile = pgTable(
-  'CustomGlossFile',
+  "CustomGlossFile",
   {
-    id: serial('id').notNull(),
-    name: text('name').notNull(),
-    url: text('url').notNull(),
-    documentType: text('documentType'),
-    customGlossId: uuid('customGlossId').notNull(),
-    createdAt: timestamp('createdAt', { precision: 3 }).notNull().defaultNow(),
+    id: serial("id").notNull(),
+    name: text("name").notNull(),
+    url: text("url").notNull(),
+    documentType: text("documentType"),
+    customGlossId: uuid("customGlossId").notNull(),
+    createdAt: timestamp("createdAt", { precision: 3 }).notNull().defaultNow(),
   },
   (CustomGlossFile) => ({
     CustomGlossFile_customGloss_fkey: foreignKey({
-      name: 'CustomGlossFile_customGloss_fkey',
+      name: "CustomGlossFile_customGloss_fkey",
       columns: [CustomGlossFile.customGlossId],
       foreignColumns: [CustomGloss.id],
     })
-      .onDelete('cascade')
-      .onUpdate('cascade'),
+      .onDelete("cascade")
+      .onUpdate("cascade"),
     CustomGlossFile_cpk: primaryKey({
-      name: 'CustomGlossFile_cpk',
+      name: "CustomGlossFile_cpk",
       columns: [CustomGlossFile.id, CustomGlossFile.customGlossId],
     }),
   })
@@ -116,27 +114,27 @@ export const CustomGlossFile = pgTable(
 export type CustomGlossFileTable = typeof CustomGlossFile.$inferSelect;
 
 export const CustomGlossTab = pgTable(
-  'CustomGlossTab',
+  "CustomGlossTab",
   {
-    id: uuid('id').notNull().primaryKey().defaultRandom(),
-    name: text('name').notNull(),
-    summary: text('summary'),
-    isCorrect: boolean('isCorrect').notNull(),
-    fullContext: boolean('fullContext').notNull(),
-    isVerified: boolean('isVerified').notNull(),
-    customGlossId: uuid('customGlossId').notNull(),
-    createdAt: timestamp('createdAt', { precision: 3 }).notNull().defaultNow(),
+    id: uuid("id").notNull().primaryKey().defaultRandom(),
+    name: text("name").notNull(),
+    summary: text("summary"),
+    isCorrect: boolean("isCorrect").notNull(),
+    fullContext: boolean("fullContext").notNull(),
+    isVerified: boolean("isVerified").notNull(),
+    customGlossId: uuid("customGlossId").notNull(),
+    createdAt: timestamp("createdAt", { precision: 3 }).notNull().defaultNow(),
   },
   (CustomGlossTab) => ({
     CustomGlossTab_customGloss_fkey: foreignKey({
-      name: 'CustomGlossTab_customGloss_fkey',
+      name: "CustomGlossTab_customGloss_fkey",
       columns: [CustomGlossTab.customGlossId],
       foreignColumns: [CustomGloss.id],
     })
-      .onDelete('cascade')
-      .onUpdate('cascade'),
+      .onDelete("cascade")
+      .onUpdate("cascade"),
     CustomGlossTab_name_customGlossId_unique_idx: uniqueIndex(
-      'CustomGlossTab_name_customGlossId_key'
+      "CustomGlossTab_name_customGlossId_key"
     ).on(CustomGlossTab.name, CustomGlossTab.customGlossId),
   })
 );
@@ -144,23 +142,24 @@ export const CustomGlossTab = pgTable(
 export type CustomGlossTabTable = typeof CustomGlossTab.$inferSelect;
 
 export const CustomGlossTabContext = pgTable(
-  'CustomGlossTabContext',
+  "CustomGlossTabContext",
   {
-    id: serial('id').notNull().primaryKey(),
-    type: CustomGlossTabContextType('type').notNull(),
-    origin: text('origin').notNull(),
-    summary: text('summary'),
-    customGlossTabId: uuid('customGlossTabId').notNull(),
-    createdAt: timestamp('createdAt', { precision: 3 }).notNull().defaultNow(),
+    id: serial("id").notNull().primaryKey(),
+    type: CustomGlossTabContextType("type").notNull(),
+    origin: text("origin").notNull(),
+    summary: text("summary"),
+    url: text("url").notNull(),
+    customGlossTabId: uuid("customGlossTabId").notNull(),
+    createdAt: timestamp("createdAt", { precision: 3 }).notNull().defaultNow(),
   },
   (CustomGlossTabContext) => ({
     CustomGlossTabContext_customGlossTab_fkey: foreignKey({
-      name: 'CustomGlossTabContext_customGlossTab_fkey',
+      name: "CustomGlossTabContext_customGlossTab_fkey",
       columns: [CustomGlossTabContext.customGlossTabId],
       foreignColumns: [CustomGlossTab.id],
     })
-      .onDelete('cascade')
-      .onUpdate('cascade'),
+      .onDelete("cascade")
+      .onUpdate("cascade"),
   })
 );
 
@@ -168,22 +167,22 @@ export type CustomGlossTabContextTable =
   typeof CustomGlossTabContext.$inferSelect;
 
 export const CustomGlossTabContextData = pgTable(
-  'CustomGlossTabContextData',
+  "CustomGlossTabContextData",
   {
-    id: serial('id').notNull().primaryKey(),
-    name: text('name').notNull(),
-    value: text('value').notNull(),
-    customGlossTabContextId: integer('customGlossTabContextId').notNull(),
-    createdAt: timestamp('createdAt', { precision: 3 }).notNull().defaultNow(),
+    id: serial("id").notNull().primaryKey(),
+    name: text("name").notNull(),
+    value: text("value").notNull(),
+    customGlossTabContextId: integer("customGlossTabContextId").notNull(),
+    createdAt: timestamp("createdAt", { precision: 3 }).notNull().defaultNow(),
   },
   (CustomGlossTabContextData) => ({
     CustomGlossTabContextData_customGlossTabContext_fkey: foreignKey({
-      name: 'CustomGlossTabContextData_customGlossTabContext_fkey',
+      name: "CustomGlossTabContextData_customGlossTabContext_fkey",
       columns: [CustomGlossTabContextData.customGlossTabContextId],
       foreignColumns: [CustomGlossTabContext.id],
     })
-      .onDelete('cascade')
-      .onUpdate('cascade'),
+      .onDelete("cascade")
+      .onUpdate("cascade"),
   })
 );
 
@@ -191,33 +190,33 @@ export type CustomGlossTabContextDataTable =
   typeof CustomGlossTabContextData.$inferSelect;
 
 export const CustomGlossTabValidationStep = pgTable(
-  'CustomGlossTabValidationStep',
+  "CustomGlossTabValidationStep",
   {
-    id: serial('id').notNull().primaryKey(),
-    name: text('name'),
-    fraccion: text('fraccion'),
-    isCorrect: boolean('isCorrect'),
-    description: text('description'),
-    llmAnalysis: text('llmAnalysis'),
-    parentStepId: integer('parentStepId'),
-    customGlossTabId: uuid('customGlossTabId'),
-    createdAt: timestamp('createdAt', { precision: 3 }).notNull().defaultNow(),
+    id: serial("id").notNull().primaryKey(),
+    name: text("name"),
+    fraccion: text("fraccion"),
+    isCorrect: boolean("isCorrect"),
+    description: text("description"),
+    llmAnalysis: text("llmAnalysis"),
+    parentStepId: integer("parentStepId"),
+    customGlossTabId: uuid("customGlossTabId"),
+    createdAt: timestamp("createdAt", { precision: 3 }).notNull().defaultNow(),
   },
   (CustomGlossTabValidationStep) => ({
     CustomGlossTabValidationStep_parentStep_fkey: foreignKey({
-      name: 'CustomGlossTabValidationStep_parentStep_fkey',
+      name: "CustomGlossTabValidationStep_parentStep_fkey",
       columns: [CustomGlossTabValidationStep.parentStepId],
       foreignColumns: [CustomGlossTabValidationStep.id],
     })
-      .onDelete('cascade')
-      .onUpdate('cascade'),
+      .onDelete("cascade")
+      .onUpdate("cascade"),
     CustomGlossTabValidationStep_customGlossTab_fkey: foreignKey({
-      name: 'CustomGlossTabValidationStep_customGlossTab_fkey',
+      name: "CustomGlossTabValidationStep_customGlossTab_fkey",
       columns: [CustomGlossTabValidationStep.customGlossTabId],
       foreignColumns: [CustomGlossTab.id],
     })
-      .onDelete('cascade')
-      .onUpdate('cascade'),
+      .onDelete("cascade")
+      .onUpdate("cascade"),
   })
 );
 
@@ -225,26 +224,26 @@ export type CustomGlossTabValidationStepTable =
   typeof CustomGlossTabValidationStep.$inferSelect;
 
 export const CustomGlossTabValidationStepActionToTake = pgTable(
-  'CustomGlossTabValidationStepActionToTake',
+  "CustomGlossTabValidationStepActionToTake",
   {
-    id: serial('id').notNull().primaryKey(),
-    description: text('description').notNull(),
+    id: serial("id").notNull().primaryKey(),
+    description: text("description").notNull(),
     customGlossTabValidationStepId: integer(
-      'customGlossTabValidationStepId'
+      "customGlossTabValidationStepId"
     ).notNull(),
-    createdAt: timestamp('createdAt', { precision: 3 }).notNull().defaultNow(),
+    createdAt: timestamp("createdAt", { precision: 3 }).notNull().defaultNow(),
   },
   (CustomGlossTabValidationStepActionToTake) => ({
     CustomGlossTabValidationStepActionToTake_customGlossTabValidationStep_fkey:
       foreignKey({
-        name: 'CustomGlossTabValidationStepActionToTake_customGlossTabValidationStep_fkey',
+        name: "CustomGlossTabValidationStepActionToTake_customGlossTabValidationStep_fkey",
         columns: [
           CustomGlossTabValidationStepActionToTake.customGlossTabValidationStepId,
         ],
         foreignColumns: [CustomGlossTabValidationStep.id],
       })
-        .onDelete('cascade')
-        .onUpdate('cascade'),
+        .onDelete("cascade")
+        .onUpdate("cascade"),
   })
 );
 
@@ -252,28 +251,28 @@ export type CustomGlossTabValidationStepActionToTakeTable =
   typeof CustomGlossTabValidationStepActionToTake.$inferSelect;
 
 export const CustomGlossTabValidationStepResources = pgTable(
-  'CustomGlossTabValidationStepResources',
+  "CustomGlossTabValidationStepResources",
   {
-    id: serial('id').notNull().primaryKey(),
-    name: text('name'),
-    link: text('link'),
+    id: serial("id").notNull().primaryKey(),
+    name: text("name"),
+    link: text("link"),
     customGlossTabValidationStepId: integer(
-      'customGlossTabValidationStepId'
+      "customGlossTabValidationStepId"
     ).notNull(),
-    createdAt: timestamp('createdAt', { precision: 3 }).notNull().defaultNow(),
-    updatedAt: timestamp('updatedAt', { precision: 3 }).notNull(),
+    createdAt: timestamp("createdAt", { precision: 3 }).notNull().defaultNow(),
+    updatedAt: timestamp("updatedAt", { precision: 3 }).notNull(),
   },
   (CustomGlossTabValidationStepResources) => ({
     CustomGlossTabValidationStepResources_customGlossTabValidationStep_fkey:
       foreignKey({
-        name: 'CustomGlossTabValidationStepResources_customGlossTabValidationStep_fkey',
+        name: "CustomGlossTabValidationStepResources_customGlossTabValidationStep_fkey",
         columns: [
           CustomGlossTabValidationStepResources.customGlossTabValidationStepId,
         ],
         foreignColumns: [CustomGlossTabValidationStep.id],
       })
-        .onDelete('cascade')
-        .onUpdate('cascade'),
+        .onDelete("cascade")
+        .onUpdate("cascade"),
   })
 );
 
@@ -282,13 +281,13 @@ export type CustomGlossTabValidationStepResourcesTable =
 
 export const CustomGlossRelations = relations(CustomGloss, ({ many }) => ({
   tabs: many(CustomGlossTab, {
-    relationName: 'CustomGlossToCustomGlossTab',
+    relationName: "CustomGlossToCustomGlossTab",
   }),
   files: many(CustomGlossFile, {
-    relationName: 'CustomGlossToCustomGlossFile',
+    relationName: "CustomGlossToCustomGlossFile",
   }),
   alerts: many(CustomGlossAlert, {
-    relationName: 'CustomGlossToCustomGlossAlert',
+    relationName: "CustomGlossToCustomGlossAlert",
   }),
 }));
 
@@ -296,7 +295,7 @@ export const CustomGlossAlertRelations = relations(
   CustomGlossAlert,
   ({ one }) => ({
     customGloss: one(CustomGloss, {
-      relationName: 'CustomGlossToCustomGlossAlert',
+      relationName: "CustomGlossToCustomGlossAlert",
       fields: [CustomGlossAlert.customGlossId],
       references: [CustomGloss.id],
     }),
@@ -307,7 +306,7 @@ export const CustomGlossFileRelations = relations(
   CustomGlossFile,
   ({ one }) => ({
     customGloss: one(CustomGloss, {
-      relationName: 'CustomGlossToCustomGlossFile',
+      relationName: "CustomGlossToCustomGlossFile",
       fields: [CustomGlossFile.customGlossId],
       references: [CustomGloss.id],
     }),
@@ -318,13 +317,13 @@ export const CustomGlossTabRelations = relations(
   CustomGlossTab,
   ({ many, one }) => ({
     context: many(CustomGlossTabContext, {
-      relationName: 'CustomGlossTabToCustomGlossTabContext',
+      relationName: "CustomGlossTabToCustomGlossTabContext",
     }),
     validations: many(CustomGlossTabValidationStep, {
-      relationName: 'CustomGlossTabToCustomGlossTabValidationStep',
+      relationName: "CustomGlossTabToCustomGlossTabValidationStep",
     }),
     customGloss: one(CustomGloss, {
-      relationName: 'CustomGlossToCustomGlossTab',
+      relationName: "CustomGlossToCustomGlossTab",
       fields: [CustomGlossTab.customGlossId],
       references: [CustomGloss.id],
     }),
@@ -335,10 +334,10 @@ export const CustomGlossTabContextRelations = relations(
   CustomGlossTabContext,
   ({ many, one }) => ({
     data: many(CustomGlossTabContextData, {
-      relationName: 'CustomGlossTabContextToCustomGlossTabContextData',
+      relationName: "CustomGlossTabContextToCustomGlossTabContextData",
     }),
     customGlossTab: one(CustomGlossTab, {
-      relationName: 'CustomGlossTabToCustomGlossTabContext',
+      relationName: "CustomGlossTabToCustomGlossTabContext",
       fields: [CustomGlossTabContext.customGlossTabId],
       references: [CustomGlossTab.id],
     }),
@@ -349,7 +348,7 @@ export const CustomGlossTabContextDataRelations = relations(
   CustomGlossTabContextData,
   ({ one }) => ({
     customGlossTabContext: one(CustomGlossTabContext, {
-      relationName: 'CustomGlossTabContextToCustomGlossTabContextData',
+      relationName: "CustomGlossTabContextToCustomGlossTabContextData",
       fields: [CustomGlossTabContextData.customGlossTabContextId],
       references: [CustomGlossTabContext.id],
     }),
@@ -361,22 +360,22 @@ export const CustomGlossTabValidationStepRelations = relations(
   ({ many, one }) => ({
     resources: many(CustomGlossTabValidationStepResources, {
       relationName:
-        'CustomGlossTabValidationStepToCustomGlossTabValidationStepResources',
+        "CustomGlossTabValidationStepToCustomGlossTabValidationStepResources",
     }),
     actionsToTake: many(CustomGlossTabValidationStepActionToTake, {
       relationName:
-        'CustomGlossTabValidationStepToCustomGlossTabValidationStepActionToTake',
+        "CustomGlossTabValidationStepToCustomGlossTabValidationStepActionToTake",
     }),
     steps: many(CustomGlossTabValidationStep, {
-      relationName: 'StepToParent',
+      relationName: "StepToParent",
     }),
     parentStep: one(CustomGlossTabValidationStep, {
-      relationName: 'StepToParent',
+      relationName: "StepToParent",
       fields: [CustomGlossTabValidationStep.parentStepId],
       references: [CustomGlossTabValidationStep.id],
     }),
     customGlossTab: one(CustomGlossTab, {
-      relationName: 'CustomGlossTabToCustomGlossTabValidationStep',
+      relationName: "CustomGlossTabToCustomGlossTabValidationStep",
       fields: [CustomGlossTabValidationStep.customGlossTabId],
       references: [CustomGlossTab.id],
     }),
@@ -388,7 +387,7 @@ export const CustomGlossTabValidationStepActionToTakeRelations = relations(
   ({ one }) => ({
     customGlossTabValidationStep: one(CustomGlossTabValidationStep, {
       relationName:
-        'CustomGlossTabValidationStepToCustomGlossTabValidationStepActionToTake',
+        "CustomGlossTabValidationStepToCustomGlossTabValidationStepActionToTake",
       fields: [
         CustomGlossTabValidationStepActionToTake.customGlossTabValidationStepId,
       ],
@@ -402,7 +401,7 @@ export const CustomGlossTabValidationStepResourcesRelations = relations(
   ({ one }) => ({
     customGlossTabValidationStep: one(CustomGlossTabValidationStep, {
       relationName:
-        'CustomGlossTabValidationStepToCustomGlossTabValidationStepResources',
+        "CustomGlossTabValidationStepToCustomGlossTabValidationStepResources",
       fields: [
         CustomGlossTabValidationStepResources.customGlossTabValidationStepId,
       ],
